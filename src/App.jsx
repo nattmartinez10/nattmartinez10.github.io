f252e7eb0fe5bb458e5e947176b4d3b0a2d2e841
Natalia <nattmartinez10@hotmail.com>
2025-02-08 23:44:33 -0500

    Header menu base

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import styles from './App.module.css'
+import { NavBar } from './components/Navbar/NavBar.jsx';
+
 
 function App() {
   return (
   <div className={styles.App}>
-    <NavBar/>
+    <NavBar />
   </div>
   );
 }
--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-
-import styles from "./Navbar.module"
-
+import styles from "./Navbar.module.css"
+import { getImageUrl } from '../../utils';
 export const NavBar = () => {
   return (
   <nav className={styles.navbar}>
-    <a href="/">Portofolio</a>
-    <div>
-        <ul>
-            <li><a href="#about">About</a></li>
+    <div className={styles.menu}>
+        <ul className={styles.menuItems}>
+            <li><a href="about">About</a></li>
             <li><a href="experience">Experience</a></li>
             <li><a href="projects">Projects</a></li>
             <li><a href="contact">Contact</a></li>
