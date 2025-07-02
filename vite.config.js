f3fb2af6aedb7d5eaff06463bf4a4391a2a15c79
nattmartinez10 <nattmartinez10@hotmail.com>
2025-07-02 16:28:22 -0400

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/",
+  base:"/nattmartinez10.github.io/",
   css:{
     modules: {
       localsConvention: "camelCase"
